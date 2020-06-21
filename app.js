4bedf3bb1d75e85f54abf3cdacd62539a2e09f9e
Leon Si <leonzalion@gmail.com>
2020-06-21 10:16:45 -0400

    made it working

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app = express();
 
 app.get("/", (req, res) => {
-	res.render(path.join(__dirname, "index.html"));
+	res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(process.env.PORT || 3000);

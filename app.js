574dcaac13fcc49b8cde8db34ab3c186acbf7fa2
Leon Si <leonzalion@gmail.com>
2020-06-21 18:12:57 -0400

    fixed asset loading

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname, "index.html"));
+	res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(process.env.PORT || 3000);
